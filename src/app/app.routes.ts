66a4d79b0c5fab1c4eea6409781e966097f5f3ac
S0mbr4xc <s3._xc@hotmail.com>
2024-06-15 09:13:20 -0500

    Seguimiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { RestaurantesComponent } from './pages/restaurantes/restaurantes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'inicio', pathMatch:'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'restaurantes', component: RestaurantesComponent}
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  paginas = [
+    {titulo: 'Inicio', path: 'inicio'},
+    {titulo: 'Restaurantes', path:'restaurantes'},
+    {titulo: 'Lugares de interes', path:'lugares'},
+    {titulo: 'Eventos', path:'eventos'},
+    {titulo: 'Actividades', path:'actividades'},
+    {titulo: 'Nosotros', path: 'nosotros'}
+  ]
 
+  menuOpen = true;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
